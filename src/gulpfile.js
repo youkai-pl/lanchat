694cbac18f82bbc6bdc30673175221d12034e198
akira <betat95@gmail.com>
2019-02-23 11:39:17 +0100

    changes in folder structure

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -3,13 +3,21 @@ var del = require('del');
 
 gulp.task('dist', function () {
     return new Promise(function (resolve, reject) {
+        cleanDist()
         gulp.src(['../README.md']).pipe(gulp.dest('../dist'));
         gulp.src(['../LICENSE.md']).pipe(gulp.dest('../dist'));
-        gulp.src(['../src/**/*', '!../src/**/node_modules/**']).pipe(gulp.dest('../dist'));
+        gulp.src(['../src/**/*', '!../src/**/node_modules/**', '!../src/yarn.lock']).pipe(gulp.dest('../dist'));
         resolve();
     });
 });
 
-gulp.task('clean', function (cb) {
-    return del(['../dist/**/*', '!../dist/.gitkeep'], { force: true }, cb);
+gulp.task('clean', function () {
+    return new Promise(function (resolve, reject) {
+        cleanDist();
+        resolve();
+    });
 });
+
+function cleanDist() {
+    del.sync(['../dist/**/*', '!../dist/.gitkeep'], { force: true });
+}
