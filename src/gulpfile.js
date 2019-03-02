b7618b3687b6aa7d5c1b1d42f566a47f52b95521
akira <betat95@gmail.com>
2019-03-02 14:15:46 +0100

    minify dist

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,12 +1,24 @@
-var gulp = require("gulp")
-var del = require("del")
+//import
+const gulp = require("gulp")
+const del = require("del")
+const uglify = require("gulp-uglify-es").default
+const pipeline = require("readable-stream").pipeline
+
+//gulp config
 
 gulp.task("dist", function () {
 	return new Promise(function (resolve, reject) {
 		cleanDist()
+
+		pipeline(
+			gulp.src("./lib/*.js"),
+			uglify(),
+			gulp.dest("../dist/lib/")
+		)
+
 		gulp.src(["../README.md"]).pipe(gulp.dest("../dist"))
 		gulp.src(["../LICENSE.md"]).pipe(gulp.dest("../dist"))
-		gulp.src(["../src/**/*", "!../src/**/node_modules/**", "!../src/yarn.lock"]).pipe(gulp.dest("../dist"))
+		gulp.src(["../src/**/*", "!../src/**/node_modules/**", "!../src/yarn.lock", "!../src/**lib/**"]).pipe(gulp.dest("../dist"))
 		resolve()
 	})
 })
