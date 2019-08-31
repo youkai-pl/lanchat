8eac9b91b864ff140b0f56bcfc63fd0ac7b9bd29
tofudd <betat95@gmail.com>
2019-08-31 20:22:22 +0200

    prompt input

--- lanchat/Program.cs
+++ lanchat/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using lanchat.prompt;
 
 namespace lanchat
 {
@@ -10,6 +11,7 @@ namespace lanchat
     {
         static void Main(string[] args)
         {
+            Prompt.Init();
         }
     }
 }
---
+++ lanchat/Prompt.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lanchat.prompt
+{
+    public static class Prompt
+    {
+        public static void Init()
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.FileVersion;
+
+            Console.WriteLine("Lanchat Alpha " + version);
+            Console.Write("> ");
+
+            while (true)
+            {
+                string promptInput;
+                promptInput = Console.ReadLine();
+                if (!string.IsNullOrEmpty(promptInput))
+                {
+                    Console.Write("> ");
+                } else
+                {
+                    Console.SetCursorPosition(2, Console.CursorTop-1);
+                }
+            }
+        }
+    }
+}
