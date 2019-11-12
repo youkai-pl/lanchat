f7b0d85aa6f0d57c1555d7b891702f6dfae7621c
tofudd <betat95@gmail.com>
2019-11-12 20:35:24 +0100

    shared assembly info

---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lanchat 2")]
+[assembly: AssemblyCopyright("Bartłomiej Tota (tofudd) 2019")]
+[assembly: Guid("4e74a425-a696-40fe-9b52-f03eb0144710")]
+[assembly: AssemblyVersion("2.0.0.1001")]
+[assembly: AssemblyFileVersion("2.0.0.1001")]
