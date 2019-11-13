4170979a59844d26ac106ce09f19727344e4f0f1
tofudd <betat95@gmail.com>
2019-11-13 19:17:57 +0100

    alpha build configuration, assembly info update

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,7 +2,8 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Lanchat 2")]
-[assembly: AssemblyCopyright("Bartłomiej Tota (tofudd) 2019")]
+[assembly: AssemblyCopyright("tofudd 2019")]
+[assembly: AssemblyCompany ("Copyright 2019 tofudd")]
 [assembly: Guid("4e74a425-a696-40fe-9b52-f03eb0144710")]
 [assembly: AssemblyVersion("2.0.0.1001")]
 [assembly: AssemblyFileVersion("2.0.0.1001")]
