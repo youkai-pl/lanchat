a10834aa9df13dd7339ac8d2408d1b3c0c2f66bd
tofudd <betat95@gmail.com>
2019-12-16 18:26:49 +0100

    Assembly info update

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright tofudd 2019")]
 [assembly: AssemblyCompany("tofudd")]
 [assembly: Guid("4e74a425-a696-40fe-9b52-f03eb0144710")]
-[assembly: AssemblyVersion("2.0.0.1004")]
-[assembly: AssemblyFileVersion("2.0.0.1004")]
+[assembly: AssemblyVersion("2.0.0.1005")]
+[assembly: AssemblyInformationalVersion("2.0.0 Alpha 5")]
