db89318cb63a9d7b043d1d4a23a9fe22640d15f4
tofudd <betat95@gmail.com>
2019-12-26 17:41:35 +0100

    Version number increase

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright tofudd 2019")]
 [assembly: AssemblyCompany("tofudd")]
 [assembly: Guid("4e74a425-a696-40fe-9b52-f03eb0144710")]
-[assembly: AssemblyVersion("2.0.0.1005")]
-[assembly: AssemblyInformationalVersion("2.0.0 Alpha 5")]
+[assembly: AssemblyVersion("2.0.0.1006")]
+[assembly: AssemblyInformationalVersion("2.0.0 Alpha 6")]
