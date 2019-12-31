4ee07f5b696fb0de36905a5811f6ed7cd9a42d03
tofudd <bartlomiej.tota@outlook.com>
2019-12-31 17:42:52 +0100

    Manual connect in separated thread

--- Lanchat.Cli/Commands/Connect.cs
+++ Lanchat.Cli/Commands/Connect.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Net;
+using System.Threading;
 using Lanchat.Cli.Ui;
 
 namespace Lanchat.Cli.Commands
@@ -9,27 +10,31 @@ namespace Lanchat.Cli.Commands
     {
         public void Connect(string ip, string port)
         {
-            try
+            new Thread(() =>
             {
-
-                var parsedIP = IPAddress.Parse(ip);
-                var parsedPort = int.Parse(port);
-                Prompt.Notice($"Attempting connect to {ip} on port {port}");
                 try
                 {
-                    program.Network.Connect(parsedIP, parsedPort);
+                    var parsedIP = IPAddress.Parse(ip);
+                    var parsedPort = int.Parse(port);
+                    Prompt.Notice($"Attempting connect to {ip} on port {port}");
+                    try
+                    {
+
+                        program.Network.Connect(parsedIP, parsedPort);
+
+                    }
+                    catch (Exception e)
+                    {
+                        Trace.WriteLine("Connect command error: " + e.GetType());
+                        Prompt.Alert("Manual connection failed");
+                    };
                 }
                 catch (Exception e)
                 {
                     Trace.WriteLine("Connect command error: " + e.GetType());
-                    Prompt.Alert("Manual connection failed");
-                };
-            }
-            catch (Exception e)
-            {
-                Trace.WriteLine("Connect command error: " + e.GetType());
-                Prompt.Alert("Incorrect values");
-            }
+                    Prompt.Alert("Incorrect values");
+                }
+            }).Start();
         }
     }
 }
