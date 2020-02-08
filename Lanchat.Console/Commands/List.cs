88c90957a547f12616ab450cde7ed78f10601488
tofudd <bartlomiej.tota@outlook.com>
2020-02-08 22:51:40 +0100

    Better list command

--- Lanchat.Console/Commands/List.cs
+++ Lanchat.Console/Commands/List.cs
@@ -13,15 +13,28 @@ namespace Lanchat.Console.Commands
 
             foreach (var item in program.Network.NodeList)
             {
-                nodes.Add($"{item.Nickname} ({item.Ip})");
+                nodes.Add($"{item.Nickname} ({GetStatus(item.State)})");
                 count++;
             }
 
+            Prompt.Out("");
             Prompt.Out($"Connected peers: {count}");
 
             foreach (var item in nodes)
             {
                 Prompt.Out(item);
+            }
+        }
+
+        private string GetStatus(Status status)
+        {
+            if (status == Status.Ready)
+            {
+                return "\u001b[92monline\u001b[0m";
+            }
+            else
+            {
+                return "\u001b[91offline\u001b[0m"; 
             }
         }
     }
