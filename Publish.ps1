c7f15eec8e14af390308c41c034cefe1d0a6cfeb
tofudd <bartlomiej.tota@outlook.com>
2020-02-09 18:25:14 +0100

    Merging assemblies into single executable file for Windows

--- Publish.ps1
+++ Publish.ps1
@@ -1,3 +1,5 @@
+$CurrentDir = (Get-Item -Path '.\' -Verbose)
+
 echo Cleaning
 
 rm -r -fo "Lanchat.Console\bin\Publish"
@@ -7,6 +9,18 @@ echo Building
 dotnet publish Lanchat.Console\Lanchat.Console.csproj /p:PublishProfile=Lanchat.Console\Properties\PublishProfiles\Windows.pubxml -f net472 -c Release
 dotnet publish Lanchat.Console\Lanchat.Console.csproj /p:PublishProfile=Lanchat.Console\Properties\PublishProfiles\Linux.pubxml -f netcoreapp3.0 -c Release
 dotnet publish Lanchat.Console\Lanchat.Console.csproj /p:PublishProfile=Lanchat.Console\Properties\PublishProfiles\Mac.pubxml -f netcoreapp3.0 -c Release
+
+echo Merging
+$ilmergePath = "$env:USERPROFILE\.nuget\packages\ilmerge\3.0.29\tools\net452\ILMerge.exe"
+Set-Alias ILMerge $ilmergePath
+
+$WindowsBuildPath = "$CurrentDir\Lanchat.Console\bin\Publish\Windows"
+
+ILMerge /out:"$WindowsBuildPath\Lanchat.exe" "$WindowsBuildPath\Lanchat.exe" "$WindowsBuildPath\Colorful.Console.dll" "$WindowsBuildPath\Lanchat.Common.dll" "$WindowsBuildPath\Newtonsoft.Json.dll"
+
+echo Removing unnecessary files
+
+Get-ChildItem -Path $WindowsBuildPath -Recurse | Remove-Item -Exclude "Lanchat.exe" -Recurse -force 
 
 echo Packing
 
@@ -18,7 +32,6 @@ if (-not (Test-Path -Path $7zipPath -PathType Leaf)) {
 
 Set-Alias 7zip $7zipPath
 
-$CurrentDir = (Get-Item -Path '.\' -Verbose)
 $Version = [System.Reflection.Assembly]::LoadFrom("$CurrentDir\Lanchat.Console\bin\Publish\Windows\Lanchat.exe").GetName().Version.ToString()
 
 7zip a -r "Lanchat.Console\bin\Publish\Lanchat_Windows_$Version.zip" "$CurrentDir\Lanchat.Console\bin\Publish\Windows\*"
