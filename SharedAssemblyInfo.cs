ce757186abd44c0bfc59580135f5ccc637a77df8
tofudd <bartlomiej.tota@outlook.com>
2020-02-15 22:55:31 +0100

    Version number increase

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyCopyright("Copyright tofudd (2020)")]
 [assembly: AssemblyCompany("tofudd")]
-[assembly: AssemblyVersion("2.0.0.2003")]
-[assembly: AssemblyInformationalVersion("2.0.0 Beta 3")]
+[assembly: AssemblyVersion("2.0.0.2005")]
+[assembly: AssemblyInformationalVersion("2.0.0 Beta 5")]
