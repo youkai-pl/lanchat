f0979b2bca14daa96db67d916d802284105e86e1
tofudd <bartlomiej.tota@outlook.com>
2020-03-07 23:20:31 +0100

    CA1303 and CA1822 fix in About.cs

--- Lanchat.Console/Commands/About.cs
+++ Lanchat.Console/Commands/About.cs
@@ -4,33 +4,36 @@ namespace Lanchat.Console.Commands
 {
     public partial class Command
     {
-        public void About()
+        public static void About()
         {
-            Prompt.Out("");
-            Prompt.Out("------------------------------------------------------------------------");
-            Prompt.Out("|               .,-:;//;:=,                                            |");
-            Prompt.Out("|           . :H@@@MM@M#H/.,+%;,                                       |");
-            Prompt.Out("|        ,/X+ +M@@M@MM%=,-%HMMM@X/,                                    |");
-            Prompt.Out("|      -+@MM; $M@@MH+-,;XMMMM@MMMM@+-                                  |");
-            Prompt.Out("|     ;@M@@M- XM@X;. -+XXXXXHHH@M@M#@/.                                |");
-            Prompt.Out("|   ,%MM@@MH ,@%=             .---=-=:=,.                              |");
-            Prompt.Out("|   =@#@@@MX.,                -%HX$$%%%:;                              |");
-            Prompt.Out("|  =-./@M@M$                   .;@MMMM@MM:    _____ ___  _____ _   _   |");
-            Prompt.Out("|  X@/ -$MM/                    . +MM@@@M$   |_   _/ _ \\|  ___| | | |  |");
-            Prompt.Out("| ,@M@H: :@:                    . =X#@@@@-     | || | | | |_  | | | |  |");
-            Prompt.Out("| ,@@@MMX, .                    /H- ;@M@M=     | || |_| |  _| | |_| |  |");
-            Prompt.Out("| .H@@@@M@+,                    %MM+..%#$.     |_| \\___/|_|    \\___/   |");
-            Prompt.Out("|  /MMMM@MMH/.                  XM@MH; =;                              |");
-            Prompt.Out("|   /%+%$XHH@$=              , .H@@@@MX,           LABORATORIES        |");
-            Prompt.Out("|    .=--------.           -%H.,@@@@@MX,                               |");
-            Prompt.Out("|    .%MM@@@HHHXX$$$%+- .:$MMX =M@@MM%.                                |");
-            Prompt.Out("|      =XMMM@MM@MM#H;,-+HMM@M+ /MMMX=                                  |");
-            Prompt.Out("|        =%@M@M#@$-.=$@MM@@@M; %M%=                                    |");
-            Prompt.Out("|          ,:+$+-,/H#MMMMMMM@= =,                                      |");
-            Prompt.Out("|                =++%%%%+/:-.                                          |");
-            Prompt.Out("------------------------------------------------------------------------");
-            Prompt.Out("");
-            Prompt.Out("Lanchat 2 episode 1 confirmed");
+            string[] about = new string[25];
+            about[0] = "";
+            about[1] = "------------------------------------------------------------------------";
+            about[2] = "|               .,-:;//;:=,                                            |";
+            about[3] = "|           . :H@@@MM@M#H/.,+%;,                                       |";
+            about[4] = "|        ,/X+ +M@@M@MM%=,-%HMMM@X/,                                    |";
+            about[5] = "|      -+@MM; $M@@MH+-,;XMMMM@MMMM@+-                                  |";
+            about[6] = "|     ;@M@@M- XM@X;. -+XXXXXHHH@M@M#@/.                                |";
+            about[7] = "|   ,%MM@@MH ,@%=             .---=-=:=,.                              |";
+            about[8] = "|   =@#@@@MX.,                -%HX$$%%%:;                              |";
+            about[9] = "|  =-./@M@M$                   .;@MMMM@MM:    _____ ___  _____ _   _   |";
+            about[10] = "|  X@/ -$MM/                    . +MM@@@M$   |_   _/ _ \\|  ___| | | |  |";
+            about[11] = "| ,@M@H: :@:                    . =X#@@@@-     | || | | | |_  | | | |  |";
+            about[12] = "| ,@@@MMX, .                    /H- ;@M@M=     | || |_| |  _| | |_| |  |";
+            about[13] = "| .H@@@@M@+,                    %MM+..%#$.     |_| \\___/|_|    \\___/   |";
+            about[14] = "|  /MMMM@MMH/.                  XM@MH; =;                              |";
+            about[15] = "|   /%+%$XHH@$=              , .H@@@@MX,           LABORATORIES        |";
+            about[16] = "|    .=--------.           -%H.,@@@@@MX,                               |";
+            about[17] = "|    .%MM@@@HHHXX$$$%+- .:$MMX =M@@MM%.                                |";
+            about[18] = "|      =XMMM@MM@MM#H;,-+HMM@M+ /MMMX=                                  |";
+            about[19] = "|        =%@M@M#@$-.=$@MM@@@M; %M%=                                    |";
+            about[20] = "|          ,:+$+-,/H#MMMMMMM@= =,                                      |";
+            about[21] = "|                =++%%%%+/:-.                                          |";
+            about[22] = "------------------------------------------------------------------------";
+            about[23] = "";
+            about[24] = "Lanchat 2 episode 1 confirmed";
+
+            Prompt.Out(string.Join("\n", about));
         }
     }
 }
