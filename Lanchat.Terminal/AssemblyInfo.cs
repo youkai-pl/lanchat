88bfb48deb7b8de347e63c8ab6ca1daea3bb7bfb
tofudd <bartlomiej.tota@outlook.com>
2020-04-11 22:29:35 +0200

    Added config class from old project

---
+++ Lanchat.Terminal/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lanchat")]
+[assembly: AssemblyProduct("Lanchat Terminal")]
+[assembly: Guid("88A7A4DC-140C-4C2A-A2F4-18EEF9059EFA")]
---
+++ Lanchat.Terminal/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lanchat.Terminal
+{
+    class Program
+    {
+        internal static Config Config { get; set; }
+
+        static void Main(string[] args)
+        {
+            if (Array.IndexOf(args, "-debug") > -1)
+            {
+                // Attach trace here
+            }
+        }
+    }
+}
