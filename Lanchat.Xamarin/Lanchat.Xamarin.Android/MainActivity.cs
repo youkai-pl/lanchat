787e5349f594699db875fbb98cf4a16320ea4991
tofudd <bartlomiej.tota@outlook.com>
2020-04-14 23:08:38 +0200

    Fixed dependencies

--- Lanchat.Xamarin/Lanchat.Xamarin.Android/MainActivity.cs
+++ Lanchat.Xamarin/Lanchat.Xamarin.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Essentials;
 
 namespace Lanchat.Xamarin.Droid
 {
@@ -19,13 +20,13 @@ namespace Lanchat.Xamarin.Droid
 
             base.OnCreate(savedInstanceState);
 
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
