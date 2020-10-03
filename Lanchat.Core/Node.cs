c5904150a60c1c0cfe8e5d3eefc3f874a0c0f302
tofudd <bartlomiej.tota@outlook.com>
2020-10-03 15:57:58 +0200

    Network input error catching

--- Lanchat.Core/Node.cs
+++ Lanchat.Core/Node.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Text.Json;
@@ -25,12 +26,12 @@ namespace Lanchat.Core
         // Network element properties
         public Guid Id => networkElement.Id;
         public IPEndPoint Endpoint => networkElement.Endpoint;
-        
+
         // Network element events
         public event EventHandler Connected;
         public event EventHandler Disconnected;
         public event EventHandler<SocketError> SocketErrored;
-        
+
         // Node events
         public event EventHandler<string> MessageReceived;
         public event EventHandler PingReceived;
@@ -50,31 +51,48 @@ namespace Lanchat.Core
         {
             SocketErrored?.Invoke(this, e);
         }
-        
+
         // Network input
         private void OnDataReceived(object sender, string e)
         {
-            var data = JsonSerializer.Deserialize<Wrapper>(e);
-            switch (data.Type)
+            try
             {
-                case DataTypes.Message:
-                    var message = JsonSerializer.Deserialize<Message>(data.Data.ToString());
-                    MessageReceived?.Invoke(this, message.Content);
-                    break;
-                
-                case DataTypes.Ping:
-                    PingReceived?.Invoke(this, EventArgs.Empty);
-                    break;
-                
-                default:
-                    throw new ArgumentOutOfRangeException();
+                var data = JsonSerializer.Deserialize<Wrapper>(e);
+                switch (data.Type)
+                {
+                    case DataTypes.Message:
+                        var message = JsonSerializer.Deserialize<Message>(data.Data.ToString());
+                        MessageReceived?.Invoke(this, message.Content);
+                        break;
+
+                    case DataTypes.Ping:
+                        PingReceived?.Invoke(this, EventArgs.Empty);
+                        break;
+
+                    default:
+                        Debug.WriteLine("Unknown type received");
+                        break;
+                }
+            }
+            
+            // Input errors catching
+            catch (Exception ex)
+            {
+                if (ex is JsonException || ex is ArgumentNullException)
+                {
+                    Debug.WriteLine("Invalid json received");
+                }
+                else
+                {
+                    throw;
+                }
             }
         }
-        
+
         // Node output
         public void SendMessage(string content) => networkOutput.SendMessage(content);
         public void SendPing() => networkOutput.SendPing();
-        
+
         // Network element methods
         internal bool SendAsync(string text) => networkElement.SendAsync(text);
     }
