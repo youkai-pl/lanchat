5b218a7785609e1777f307d919bff63011a292ef
tofudd <bartlomiej.tota@outlook.com>
2020-10-11 17:44:52 +0200

    Program cleanup

--- Lanchat.Terminal/Program.cs
+++ Lanchat.Terminal/Program.cs
@@ -17,20 +17,20 @@ namespace Lanchat.Terminal
         private static void Main(string[] args)
         {
             _config = Config.Load();
-            
             Core.Config.Nickname = _config.Nickname;
             Network = new P2P(_config.Port);
-            
             Network.ConnectionCreated += (sender, node) => { _ = new NodeEventsHandlers(node); };
-            Prompt.Start(_config, Network);
             Network.Start();
+
+            Prompt.Start(_config, Network);
 
             if (Array.IndexOf(args, "-debug") > -1 || Debugger.IsAttached)
             {
-                Trace.WriteLine(Resources._DebugMode);
                 Trace.Listeners.Add(new TerminalTraceListener());
+                Trace.WriteLine(Resources._DebugMode);
             }
 
+            // Save logs to file
             Trace.Listeners.Add(new FileTraceListener($"{Config.Path}{DateTime.Now:yyyy_MM_dd_HH_mm_ss}.log"));
             Trace.IndentSize = 11;
             Trace.AutoFlush = true;
