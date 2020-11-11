421a093a74b359fdad3ef7a7bc9b68defaf546d2
tofudd <bartlomiej.tota@outlook.com>
2020-11-11 19:29:35 +0100

    Publish script update

--- Lanchat.Terminal/publish.bat
+++ Lanchat.Terminal/publish.bat
@@ -5,9 +5,9 @@ dotnet publish -c Release -p:PublishSingleFile=true --self-contained false -r li
 dotnet publish -c Release -p:PublishSingleFile=true --self-contained false -r linux-arm64
 dotnet publish -c Release -p:PublishSingleFile=true --self-contained false -r osx-x64
 
-rename bin\Release\netcoreapp3.1\win-x86\publish\Lanchat.exe Lanchat_win-x86.exe
-rename bin\Release\netcoreapp3.1\win-x64\publish\Lanchat.exe Lanchat_win-x64.exe
-rename bin\Release\netcoreapp3.1\linux-x64\publish\Lanchat Lanchat_linux-x64
-rename bin\Release\netcoreapp3.1\linux-arm\publish\Lanchat Lanchat_linux-arm
-rename bin\Release\netcoreapp3.1\linux-arm64\publish\Lanchat Lanchat_linux-arm64
-rename bin\Release\netcoreapp3.1\osx-x64\publish\Lanchat Lanchat_osx-x64
+rename bin\Release\net5.0\win-x86\publish\Lanchat.exe Lanchat_win-x86.exe
+rename bin\Release\net5.0\win-x64\publish\Lanchat.exe Lanchat_win-x64.exe
+rename bin\Release\net5.0\linux-x64\publish\Lanchat Lanchat_linux-x64
+rename bin\Release\net5.0\linux-arm\publish\Lanchat Lanchat_linux-arm
+rename bin\Release\net5.0\linux-arm64\publish\Lanchat Lanchat_linux-arm64
+rename bin\Release\net5.0\osx-x64\publish\Lanchat Lanchat_osx-x64
