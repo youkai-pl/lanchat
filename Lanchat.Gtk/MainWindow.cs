a46b07ee6b11e555378333150ae450e6a38ed0d7
tofudd <bartlomiej.tota@outlook.com>
2021-01-03 15:32:55 +0100

    Basic layout

--- Lanchat.Gtk/MainWindow.cs
+++ Lanchat.Gtk/MainWindow.cs
@@ -1,16 +1,10 @@
-using System;
 using Gtk;
 using UI = Gtk.Builder.ObjectAttribute;
 
 namespace Lanchat.Gtk
 {
-    class MainWindow : Window
+    internal class MainWindow : Window
     {
-        [UI] private Label _label1 = null;
-        [UI] private Button _button1 = null;
-
-        private int _counter;
-
         public MainWindow() : this(new Builder("MainWindow.glade"))
         {
         }
@@ -18,20 +12,12 @@ namespace Lanchat.Gtk
         private MainWindow(Builder builder) : base(builder.GetObject("MainWindow").Handle)
         {
             builder.Autoconnect(this);
-
             DeleteEvent += Window_DeleteEvent;
-            _button1.Clicked += Button1_Clicked;
         }
 
-        private void Window_DeleteEvent(object sender, DeleteEventArgs a)
+        private static void Window_DeleteEvent(object sender, DeleteEventArgs a)
         {
             Application.Quit();
-        }
-
-        private void Button1_Clicked(object sender, EventArgs a)
-        {
-            _counter++;
-            _label1.Text = "Hello World! This button has been clicked " + _counter + " time(s).";
         }
     }
 }
