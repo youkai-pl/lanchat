aeb18811da54238c8af5f2f9ac131f987eb02d9a
tofudd <bartlomiej.tota@outlook.com>
2021-01-19 21:45:18 +0100

    :3

--- Lanchat.ClientCore/Config.cs
+++ Lanchat.ClientCore/Config.cs
@@ -13,7 +13,7 @@ namespace Lanchat.ClientCore
     {
         private static int _port = 3645;
         private static int _broadcastPort = 3646;
-        private static string _nickname = "user";
+        private static string _nickname = NicknamesGenerator.GimmeNickname();
         private static List<string> _blockedAddresses = new();
         private static bool _automaticConnecting = true;
 
---
+++ Lanchat.ClientCore/NicknamesGenerator.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Lanchat.ClientCore
+{
+    public static class NicknamesGenerator
+    {
+        private static readonly string[] Nicknames = new[]
+        {
+            "Reimu",
+            "Marisa",
+            "Alice",
+            "Patchouli",
+            "Cirno",
+            "Sakuya",
+            "Flandre",
+            "Youmu",
+            "Ran",
+            "Yukari",
+            "Reisen",
+            "Kaguya",
+            "Mokou",
+            "Eirin",
+            "Sanae",
+            "Suwako",
+            "Utsuho",
+            "Koishi",
+            "Byakuren",
+            "Seiga",
+        };
+        
+        public static string GimmeNickname()
+        {
+            var random = new Random();
+            return Nicknames[random.Next(0, Nicknames.Length)];
+        }
+    }
+}
