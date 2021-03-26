4aaf0c96a0f56328a0382dd2397985f4b5dd416e
tofudd <bartlomiej.tota@outlook.com>
2021-03-26 16:54:51 +0100

    Reconnecting in P2P handlers

--- Lanchat.Core/P2PHandlers/P2PInternalHandlers.cs
+++ Lanchat.Core/P2PHandlers/P2PInternalHandlers.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Threading;
 using Lanchat.Core.Models;
 
 namespace Lanchat.Core.P2PHandlers
@@ -8,6 +9,7 @@ namespace Lanchat.Core.P2PHandlers
     {
         private readonly IConfig config;
         private readonly P2P network;
+        private int reconnect;
 
         internal P2PInternalHandlers(P2P network, IConfig config)
         {
@@ -18,13 +20,20 @@ namespace Lanchat.Core.P2PHandlers
         internal void CloseNode(object sender, EventArgs e)
         {
             var node = (Node) sender;
+            var address = node.NetworkElement.Endpoint.Address;
             network.OutgoingConnections.Remove(node);
             node.Dispose();
+
+            if (reconnect == 3) return;
+            Thread.Sleep(1000);
+            network.Connect(address);
+            reconnect++;
         }
 
         internal void OnConnected(object sender, EventArgs e)
         {
             var node = (Node) sender;
+            reconnect = 0;
             var nodesList = new NodesList();
             nodesList.AddRange(network.Nodes.Where(x => x.Id != node.Id)
                 .Select(x => x.NetworkElement.Endpoint.Address.ToString()));
