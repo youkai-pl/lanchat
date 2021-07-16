85cff16782299113092349a7947cd16c6ee17993
tof4 <bartlomiej.tota@outlook.com>
2021-07-16 15:38:26 +0200

    Scrollable file transfer view

--- Lanchat.Terminal/UserInterface/Views/FileTransfersView.cs
+++ Lanchat.Terminal/UserInterface/Views/FileTransfersView.cs
@@ -1,16 +1,26 @@
+using System;
 using ConsoleGUI.Controls;
+using ConsoleGUI.Data;
 using ConsoleGUI.UserDefined;
 using Lanchat.Terminal.UserInterface.Controls;
 
 namespace Lanchat.Terminal.UserInterface.Views
 {
-    public class FileTransfersView : SimpleControl
+    public class FileTransfersView : SimpleControl, IScrollable
     {
         private readonly VerticalStackPanel stackPanel = new();
         public int Counter { get; private set; } = 1;
         public FileTransfersView()
         {
-            Content = stackPanel;
+            ScrollPanel = new VerticalScrollPanel
+            {
+                Content = stackPanel,
+                ScrollBarBackground = new Character(),
+                ScrollBarForeground = new Character(),
+                ScrollUpKey = ConsoleKey.PageUp,
+                ScrollDownKey = ConsoleKey.PageDown
+            };
+            Content = ScrollPanel;
         }
 
         public void Add(FileTransferStatus fileTransferStatus)
@@ -18,5 +28,7 @@ namespace Lanchat.Terminal.UserInterface.Views
             stackPanel.Add(fileTransferStatus);
             Counter++;
         }
+
+        public VerticalScrollPanel ScrollPanel { get; }
     }
 }
