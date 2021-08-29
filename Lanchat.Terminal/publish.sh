5487ed302b14b9f29a95063db766106eaffd0f5d
tof4 <bartlomiej.tota@outlook.com>
2021-08-29 16:17:11 +0200

    Simplified publish script

--- Lanchat.Terminal/publish.sh
+++ Lanchat.Terminal/publish.sh
@@ -1,16 +1,15 @@
 rm -r bin/Packages/
 mkdir bin/Packages
 
-dotnet publish -c Release -p:PublishSingleFile=true --self-contained true -r win-x86
-dotnet publish -c Release -p:PublishSingleFile=true --self-contained true -r win-x64
-dotnet publish -c Release -p:PublishSingleFile=true --self-contained true -r linux-x64
-dotnet publish -c Release -p:PublishSingleFile=true --self-contained true -r linux-arm
-dotnet publish -c Release -p:PublishSingleFile=true --self-contained true -r linux-arm64
-dotnet publish -c Release -p:PublishSingleFile=true  --self-contained true -r osx-x64
+for rid in win-x86 win-x64 linux-x64 linux-arm linux-arm64 osx-x64;
+do
+    dotnet publish \
+    -c Release \
+    -p:PublishSingleFile=true \
+    -p:DebugType=None \
+    -p:DebugSymbols=false \
+    -p:GenerateDocumentationFile=false \
+    --self-contained true -r $rid
 
-zip -r -j bin/Packages/win-x86.zip bin/Release/net5.0/win-x86/publish/
-zip -r -j bin/Packages/win-x64.zip bin/Release/net5.0/win-x64/publish/
-zip -r -j bin/Packages/linux-x64.zip bin/Release/net5.0/linux-x64/publish/
-zip -r -j bin/Packages/linux-arm.zip bin/Release/net5.0/linux-arm/publish/
-zip -r -j bin/Packages/linux-arm64.zip bin/Release/net5.0/linux-arm64/publish/
-zip -r -j bin/Packages/osx-x64.zip bin/Release/net5.0/osx-x64/publish/
+    zip -r -j bin/Packages/$rid.zip bin/Release/net5.0/$rid/publish/
+done
