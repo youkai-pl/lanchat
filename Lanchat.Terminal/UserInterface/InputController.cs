bdf801f8180666d51a2d610f7b47ce1e2ba90ac9
tof4 <bartlomiej.tota@outlook.com>
2021-10-15 21:37:12 +0200

    History with length limit

--- Lanchat.Terminal/UserInterface/InputController.cs
+++ Lanchat.Terminal/UserInterface/InputController.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using ConsoleGUI.Controls;
 using ConsoleGUI.Input;
 using Lanchat.Terminal.Commands;
@@ -10,6 +12,9 @@ namespace Lanchat.Terminal.UserInterface
     {
         private readonly CommandsController commandsController;
         private readonly TextBox promptInput;
+        private const int HistoryLimit = 50;
+        private readonly List<string> history = new();
+        private int currentHistoryItem = HistoryLimit;
 
         public InputController(TextBox promptInput)
         {
@@ -25,13 +30,13 @@ namespace Lanchat.Terminal.UserInterface
             switch (key)
             {
                 case ConsoleKey.UpArrow:
-                    Window.UiAction(()=> promptInput.Text = "UP");
+                    Window.UiAction(()=> promptInput.Text = GetFromHistory(-1));
                     Window.UiAction(()=> promptInput.Caret = promptInput.Text.Length);
                     inputEvent.Handled = true;
                     break;
                 
                 case ConsoleKey.DownArrow:
-                    Window.UiAction(()=> promptInput.Text = "DOWN");
+                    Window.UiAction(()=> promptInput.Text = GetFromHistory(1));
                     Window.UiAction(()=> promptInput.Caret = promptInput.Text.Length);
                     inputEvent.Handled = true;
                     break;
@@ -59,10 +64,12 @@ namespace Lanchat.Terminal.UserInterface
             if (promptInput.Text.StartsWith("/", StringComparison.CurrentCulture))
             {
                 commandsController.ExecuteCommand(promptInput.Text.Split(' '));
+                AddToHistory(promptInput.Text);
             }
             else if (Window.TabPanel.CurrentTab.Content is ChatView chatView)
             {
                 SendMessage(chatView);
+                AddToHistory(promptInput.Text);
             }
         }
 
@@ -77,6 +84,30 @@ namespace Lanchat.Terminal.UserInterface
             {
                 chatView.Node.Messaging.SendPrivateMessage(promptInput.Text);
             }
+        }
+
+        private void AddToHistory(string input)
+        {
+            history.Add(input);
+            currentHistoryItem = HistoryLimit;
+            if (history.Count > HistoryLimit)
+            {
+                history.Remove(history.First());
+            }
+        }
+
+        private string GetFromHistory(int direction)
+        {
+            currentHistoryItem += direction;
+            var historyItem = history.ElementAtOrDefault(currentHistoryItem);
+            
+            if (historyItem != null)
+            {
+                return historyItem;
+            }
+
+            currentHistoryItem -= direction;
+            return promptInput.Text;
         }
     }
 }
