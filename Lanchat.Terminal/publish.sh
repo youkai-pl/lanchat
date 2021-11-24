c2e8786cda3d4f4f11026386835b3dfcc278bf60
tof4 <bartlomiej.tota@outlook.com>
2021-11-24 18:37:25 +0100

    Project files update

--- Lanchat.Terminal/publish.sh
+++ Lanchat.Terminal/publish.sh
@@ -5,11 +5,11 @@ for rid in win-x86 win-x64 linux-x64 linux-arm linux-arm64 osx-x64;
 do
     dotnet publish \
     -c Release \
-    -p:PublishSingleFile=true \
     -p:DebugType=None \
     -p:DebugSymbols=false \
     -p:GenerateDocumentationFile=false \
-    --self-contained true -r $rid
+    --self-contained true \
+    -r $rid
 
     zip -r -j bin/Packages/$rid.zip bin/Release/net6.0/$rid/publish/
 done
