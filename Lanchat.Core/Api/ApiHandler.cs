c1a12c99358a50aaae34ff16456f1445a0858afd
tof4 <bartlomiej.tota@outlook.com>
2021-12-06 12:14:00 +0100

    ApiHandler docs

--- Lanchat.Core/Api/ApiHandler.cs
+++ Lanchat.Core/Api/ApiHandler.cs
@@ -3,57 +3,72 @@ using System;
 namespace Lanchat.Core.Api
 {
     /// <summary>
-    ///     Inherit this class for create custom API handler.
+    ///     Base class for creating custom types API handlers.
     /// </summary>
-    /// <typeparam name="T">Type of handled model</typeparam>
+    /// <remarks>
+    ///     Custom API handlers can be used to create features not present in standard Lanchat.Core but using its architecture.
+    /// </remarks>
+    /// <example>
+    /// <code>
+    ///     public class PixelHandler : ApiHandler&lt;Pixel&gt;
+    ///     {
+    ///         protected override void Handle(Pixel pixel)
+    ///         {
+    ///             Main.Canvas.AddPixel(pixel);
+    ///         }
+    ///     }
+    /// </code>
+    ///     The cxample comes from <see href="https://github.com/tof4/Lanpaint/">Lanpaint</see>.
+    /// </example>
+    /// <typeparam name="T">Data model class.</typeparam>
     public abstract class ApiHandler<T> : IApiHandler
-    {
-        /// <inheritdoc />
-        public Type HandledType { get; } = typeof(T);
-
-        /// <inheritdoc />
-        public bool Privileged { get; protected init; }
-
-        /// <inheritdoc />
-        public bool Disabled { get; protected set; }
-
-        /// <inheritdoc />
-        public void Handle(object data)
-        {
-            Handle((T)data);
-        }
-
-        /// <summary>
-        ///     Handle object converted to required type.
-        /// </summary>
-        /// <param name="data">Converted object.</param>
-        protected abstract void Handle(T data);
-    }
-
-    /// <summary>
-    ///     Use <see cref="ApiHandler{T}" /> instead this.
-    /// </summary>
-    public interface IApiHandler
     {
         /// <summary>
         ///     Type of handled model.
         /// </summary>
-        public Type HandledType { get; }
+        public Type HandledType { get; } = typeof(T);
 
         /// <summary>
-        ///     If handler is privileged it will process data even if node is unready.
+        ///     Priviliged handlers will be called event if Node is not ready.
         /// </summary>
-        public bool Privileged { get; }
+        /// <remarks>
+        ///     This property is set for handlers that are active before the end of the connection process.
+        ///     For example Handshake and KeyInfo are set as privileged;
+        /// </remarks>
+        public bool Privileged { get; protected init; }
 
         /// <summary>
-        ///     Disable handler.
+        ///     If set to true Resolver will ignore incomming data for specified handler.
         /// </summary>
-        public bool Disabled { get; }
+        public bool Disabled { get; protected set; }
 
         /// <summary>
-        ///     Object handler.
+        ///     Internal handler method. Should be used only for tests.
         /// </summary>
-        /// <param name="data">API model object.</param>
-        public void Handle(object data);
+        /// <remarks>
+        ///     For some reason this have to be public. Please use Handle(T data) instead.
+        /// </remarks>
+        /// <param name="data">Model of unkown type.</param>
+        public void Handle(object data) => Handle((T)data);
+
+        /// <summary>
+        ///     Handle received data.
+        /// </summary>
+        /// <remakrs>
+        ///     Override this method to handle incomming data of specified type.
+        /// </remakrs>
+        /// <param name="data">Model.</param>
+        protected abstract void Handle(T data);
+    }
+
+    internal interface IApiHandler
+    {
+        Type HandledType { get; }
+
+        bool Privileged { get; }
+
+        bool Disabled { get; }
+
+        void Handle(object data);
     }
 }
