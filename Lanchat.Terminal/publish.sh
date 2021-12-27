eaa99de8b168a1cee6d76122dca37594d8c0956d
tof4 <bartlomiej.tota@outlook.com>
2021-12-27 22:21:37 +0100

    Trimmed binaries

--- Lanchat.Terminal/publish.sh
+++ Lanchat.Terminal/publish.sh
@@ -3,7 +3,6 @@ runtimes=(win-x86 win-x64 linux-x64 linux-arm linux-arm64 osx-x64 osx-arm64)
 rm -r bin/Packages/
 mkdir bin/Packages
 
-# Build self contained packages
 for rid in ${runtimes[@]};
 do
     dotnet publish \
@@ -14,19 +13,5 @@ do
     --self-contained \
     -r $rid
 
-    zip -r -j bin/Packages/Lanchat-$rid-Self-Contained.zip bin/Release/net6.0/$rid/publish/
-done
-
-# Build runtime dependent packages
-for rid in ${runtimes[@]};
-do
-    dotnet publish \
-    -c Release \
-    -p:DebugType=None \
-    -p:DebugSymbols=false \
-    -p:GenerateDocumentationFile=false \
-    --no-self-contained \
-    -r $rid
-
-    zip -r -j bin/Packages/Lanchat-$rid-Runtime-Dependent.zip bin/Release/net6.0/$rid/publish/
+    zip -r -j bin/Packages/Lanchat-$rid.zip bin/Release/net6.0/$rid/publish/
 done
